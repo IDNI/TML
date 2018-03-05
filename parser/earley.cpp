5e93b06a75c629838750ea0b12f5897875f8e443
Ohad Asor <ohad@idni.org>
2018-03-05 01:27:23 +0100

    few fixes

--- parser/earley.cpp
+++ parser/earley.cpp
@@ -35,16 +35,14 @@ public:
 	map<T, set<T>> in, out;
 	size_t sz = 0;
 	void add(const T& x, const T& y) {
-		if (out[x].emplace(y).second) in[y].emplace(x), ++sz;
+		if (out[x].emplace(y).second) in[y].emplace(x), out[y], ++sz;
 	}
 	bool has(const T& x, const T& y) {
 		auto it = out.find(x);
 		return it != out.end() && it->second.find(y) != it->second.end();
 	}
 	bool has_outgoing(const T& t) const { return out.find(t) != out.end(); }
-	bool has_incoming(const T& t) const { return in.find(t) != in.end(); }
 	const set<T>& outgoing(const T& t) const { return out.at(t); }
-	const set<T>& incoming(const T& t) const { return in.at(t); }
 	void close() {
 		for (size_t s = 0; s != sz;) {
 			s = sz;
@@ -72,27 +70,19 @@ class cfg {
 		size_t r = distance(g.begin(), lower_bound(g.begin(), g.end(), rule{nt}));
 		return r;
        	}
-	void add_item(size_t d, size_t i, size_t k) {
-		wcout << L"add_item(" << ei2str(d) << L", " << i << L")" << endl;
-		size_t rn = d / w, dot = d % w;
-		if (find(g[rn][dot]) < g.size())
-			(d%w == g[d/w].size() ? ec : ep)[k].emplace(len*d + i);
-		else if (samechar(in[k], g[rn][dot]))
-			add_item(d + 1, i, k + 1);
-	};
-	void print_cache(const dig<size_t>& d) const;
+	void add_item(size_t d, size_t i, size_t k);
+	void print_cache(const dig<size_t>& d) const {
+		for (auto x : d.out) {
+			wcout << dr2str(x.first) << L" => ";
+			for (auto y : x.second) wcout << dr2str(y) << L" ";
+			wcout << endl;
+		}
+	}
 public:
 	cfg(const vector<rule>&, const wstring&);
 	void operator()(const wstring& in);
 };
 ////////////////////////////////////////////////////////////////////////////////
-void cfg::print_cache(const dig<size_t>& d) const {
-	for (auto x : d.out) {
-		wcout << dr2str(x.first) << L" => ";
-		for (auto y : x.second) wcout << dr2str(y) << L" ";
-		wcout << endl;
-	}
-}
 cfg::cfg(const vector<rule>& _g, const wstring& S) : g(_g),
 	w(max_element(g.begin(), g.end(), [](const rule& x, const rule& y) {
 			return x.size() < y.size(); })->size() + 1) {
@@ -101,13 +91,26 @@ cfg::cfg(const vector<rule>& _g, const wstring& S) : g(_g),
 	for (size_t i = 0, j, k; i < g.size(); ++i)
 		for (j = 1; j < g[i].size(); ++j)
 			if ((k = find(g[i][j])) == g.size()) p.add(i*w+j,i*w+j);
-			else for (;k < g.size() && g[k][0] == g[i][j]; ++k)
-				p.add(i * w + j, k * w + 1),
-				c.add(k * w + g[k].size(), i * w + j);
+			else for (;k < g.size() && g[k][0] == g[i][j]; ++k) {
+				p.add(i * w + j, k * w + 1);
+				if (g[k].back().size())
+					c.add(k * w + g[k].size(), i*w+j+1);
+				else	c.add(k * w + g[k].size()-1, i*w+j+1);
+			}
 	wcout << L"predictor cache:" << endl; print_cache(p);
 	wcout << L"completer cache:" << endl; print_cache(c);
-	p.close();
+	p.close(), c.close();
 	wcout << L"predictor tc:" << endl; print_cache(p);
+	wcout << L"completer tc:" << endl; print_cache(c);
+}
+void cfg::add_item(size_t d, size_t i, size_t k) {
+	//wcout << L"add_item(" << dr2str(d) << L", " << i << L")" << endl;
+	if (d%w==g[d/w].size() || !g[d/w][d%w].size())
+		ec[k].emplace(len*d+i);
+	else if (find(g[d / w][d % w]) < g.size())
+		ep[k].emplace(len * d + i);
+	else if (samechar(in[k], g[d / w][d % w]))
+		add_item(d + 1, i, k + 1);
 }
 void cfg::operator()(const wstring& _in) {
 	len = (in = _in).size() + 1;
@@ -115,28 +118,38 @@ void cfg::operator()(const wstring& _in) {
 	ep = new set<size_t>[in.size() + 1];
 	ec = new set<size_t>[in.size() + 1];
 	// eitem(d, i) to size_t: d*|a|+i i=e%|a| d=e/|a|
-	add_item(w * find(L"S'"), 0, 0); 
+	add_item(1 + w * find(L"S'"), 0, 0); 
 	for (n = 0; n < len; ++n) {
-		wcout << es2str(n);
+//		wcout << es2str(n) << endl;
 		size_t sp, sc;
 		do {
 		sp = ep[n].size();
 		sc = ec[n].size();
-		for (size_t d : ep[n]) // predict
+		for (size_t d : ep[n]) { // predict
+			wcout << L"predicting " << ei2str(d) << endl;
 			if (p.has_outgoing(d / len))
 				for (size_t t : p.outgoing(d / len))
-					add_item(t * w + 1, n, n);
-		for (size_t d : ec[n]) // complete
+					wcout << ei2str(d) <<
+						L" has p.outgoing " <<
+						dr2str(t) << endl,
+					add_item(t, n, n);
+		}
+		for (size_t d : ec[n]) { // complete
+			wcout << L"completing " << ei2str(d) << endl;
 			if (c.has_outgoing(d / len))
-				for (size_t t : c.outgoing(d / len))
-					for (auto it=ep[d/len].lower_bound(t);
+				for (size_t t : c.outgoing(d / len)) {
+					wcout << ei2str(d) << L" has c.outgoing " << dr2str(t) << endl;
+					for (auto it=ep[d%len].lower_bound(t-1);
 						it != ep[d / len].end() &&
-						*it == t; ++it)
-						add_item(w*(*it%len)+*it/len+1
-								, d%len, n);
+						*it == t-1; ++it)
+						add_item(*it%len, d%len, n);
+				}
+		}
 		} while (sp != ep[n].size() && sc != ec[n].size());
 		wcout << es2str(n);
 	}
+	for (n = 0; n < len; ++n)
+		wcout << es2str(n);
 	delete[] es;
 	delete[] ep;
 	delete[] ec;
@@ -145,8 +158,8 @@ void cfg::operator()(const wstring& _in) {
 wstring cfg::dr2str(size_t d) const {
 	const rule& r = g[d / w];
 	wstringstream ss;
-	ss << L'[' << r[0] << L" -> ";
 	if (!(d % w)) ss << L"* ";
+	ss << L'[' << r[0] << L" -> ";
 	for (size_t n = 1; n < r.size(); ++n) {
 		if (n == d % w) ss << L"* ";
 		ss << r[n] << L' ';
@@ -162,9 +175,9 @@ wstring cfg::ei2str(size_t i) const {
 wstring cfg::es2str(size_t s) const {
 	wstringstream ss;
 	ss << L"E[" << s << L"]" << endl;
-	for (size_t i : es[s]) ss << ei2str(i) << endl;
-	for (size_t i : ep[s]) ss << ei2str(i) << endl;
-	for (size_t i : ec[s]) ss << ei2str(i) << endl;
+	for (size_t i : es[s]) ss << L's' << ei2str(i) << endl;
+	for (size_t i : ep[s]) ss << L'p' << ei2str(i) << endl;
+	for (size_t i : ec[s]) ss << L'c' << ei2str(i) << endl;
 	return ss.str();
 }
 ////////////////////////////////////////////////////////////////////////////////
@@ -172,12 +185,13 @@ int main(int, char**) {
 	setlocale(LC_ALL, "");
 	wstring S(L"S"), T(L"T"), B(L"B"), a(L"a"), b(L"b"), ws(L"ws"), eps,
 		A(L"A");
-	cfg g({ { S, S, T },
-		{ S, a },
-		{ B, eps },
-		{ T, a, b },
-		{ T, a }
-		}, S);
-	g(L"aa");
+	cfg g({ { S, a, S }, { S, eps }}, S);
+//	cfg g({ { S, S, T },
+//		{ S, a },
+//		{ B, eps },
+//		{ T, a, b },
+//		{ T, a }
+//		}, S);
+	g(L"a");
 	return 0;
 }
