b078a289cf2b9b14e87fc88d0dbb56f85aaf226d
Ohad Asor <ohad@idni.org>
2018-03-19 19:19:35 +0100

    few more fixes

--- parser/earley.cpp
+++ parser/earley.cpp
@@ -10,7 +10,6 @@
 #include <sstream>
 using namespace std;
 
-typedef array<size_t, 2> coord;
 typedef array<size_t, 2> interval;
 enum gitem_t { NULLABLE, TERMINAL, NONTERM };
 #define subset(x, y) includes(x.begin(), x.end(), y.begin(), y.end())
@@ -26,14 +25,12 @@ typedef pair<vector<vector<gitem>>, map<size_t, const wchar_t*>> compiled_cfg;
 wstring format(const compiled_cfg& g, size_t i, size_t j) {
 	wstringstream ss;
 	gitem t;
-	if (j < g.first[i].size()) t = g.first[i][j];
 	for (size_t n = 0; n < g.first[i].size(); ++n) {
+		t = g.first[i][n];
 		if (n == j) ss << "* ";
-		if (j < g.first[i].size()) {
-			if (t.t==TERMINAL) ss << t.ch;
-			else ss << g.second.at(t.i[0]) << ' ';
-		}
+		t.t==TERMINAL ? ss<<t.ch : ss<<g.second.at(t.i[0])<<' ';
 	}
+	if (j == g.first[i].size()) ss << "* ";
 	return ss.str();
 }
 
@@ -42,8 +39,6 @@ compiled_cfg cfg_compile(vector<vector<wstring>> g, wstring S) {
 	set<wstring> nulls;
 	map<wstring, interval> alts;
 	map<wstring, set<wstring>> in, out;
-	map<wstring, set<wstring>>::const_iterator it, jt;
-	map<wstring, interval>::const_iterator kt;
 	wstring Z = L"Z";
 	size_t s, i = 0, j = 0;
 
@@ -51,25 +46,28 @@ compiled_cfg cfg_compile(vector<vector<wstring>> g, wstring S) {
 	nulls.emplace(), sort(g.begin(), g.end()), r.first.resize(g.size() + 1);
 	while (Z <= g[g.size()-1][0]) Z += L'Z';
 	g.push_back({Z, S});
-	//wcout<<g.size()<<endl;
+
 	for (bool b = false; !b; ++i)
 		if ((b = (i == g.size() - 1)) || g[i][0] != g[i+1][0])
 			alts.emplace(g[i][0], interval{{j, i + 1}}), j = i + 1;
 	for (auto a : alts) r.second.emplace(a.second[0], wcsdup(a.first.c_str()));
+
 	for (i = 0; i < g.size(); ++i)
 		for (r.first[i].resize(g[i].size() - 1), j = 1; j < g[i].size(); ++j)
 			out[g[i][0]].emplace(g[i][j]),
 			in[g[i][j]].emplace(g[i][0]);
-iter:	s = r.first.size();
+
+iter:	s = nulls.size();
 	for (const wstring& t : nulls)
-		if ((it = in.find(t)) == in.end()) continue;
+		if (auto it = in.find(t); it == in.end()) continue;
 		else for (const wstring& y : it->second)
-			if (	(jt = out.find(y)) != out.end() &&
+			if (auto jt = out.find(y); jt != out.end() &&
 				subset(jt->second, nulls)) nulls.emplace(y);
-	if (s != r.first.size()) goto iter;
+	if (s != nulls.size()) goto iter;
+
 	for (i = 0; i < g.size(); ++i)
 		for (j = 1; j < g[i].size(); ++j)
-			if ((kt = alts.find(g[i][j])) != alts.end())
+			if (auto kt = alts.find(g[i][j]); kt != alts.end())
 				r.first[i][j-1].set(kt->second,
 					nulls.find(g[i][j]) != nulls.end());
 			else r.first[i][j-1].set(g[i][j][0]);
@@ -83,7 +81,7 @@ struct eitem {
 	eitem(const vector<vector<gitem>>& g, size_t end, size_t len,
 		size_t alt, size_t dot):eitem(end, g[alt].size()==dot? UINT_MAX:
 			g[alt][dot].t != TERMINAL ? g[alt][dot].i[0] : UINT_MAX
-			, len, alt, dot) {}
+			, len, alt, dot) { }
 };
 
 #define cmp_(t) x.t!=y.t
@@ -100,19 +98,20 @@ wstring format(const compiled_cfg& g, eitem i) {
 	return ss.str();
 }
 
-bool cfg_parse(const compiled_cfg& G, const wchar_t* in) {
-#define add_item(i, j) outs[i].emplace(j), ins[j].emplace(i), front.emplace(j)
-	auto &g = G.first;
-	eitem i(g, 0, 0, g.size() - 1, 0), j, k;
-	set<eitem> front;
-	map<eitem, set<eitem>> ins, outs;
-	set<eitem> s;
-	map<eitem, set<eitem>>::const_iterator it, jt;
-	gitem x;
 
+bool cfg_parse(const compiled_cfg& G, const wchar_t* in) {
+	eitem i(G.first, 0, 0, G.first.size() - 1, 0), j, k;
+	set<eitem> front, s;
+	map<eitem, set<eitem>> ins, outs;
+	gitem x;
+	auto &g = G.first;
+
+#define add_item(i, j) outs[i].emplace(j), ins[j].emplace(i), front.emplace(j), front.erase(i)
 start:	wcout << format(G, i) << endl;
-	if (i.nt != UINT_MAX) {
-		for (it = outs.lower_bound(eitem(i.end - i.len, i.nt, 0, 0, 0));
+	assert(i.alt < g.size() && i.dot <= g[i.alt].size());
+	if (g[i.alt].size() <= i.dot) {
+		if (i.nt != UINT_MAX)
+		for (auto it = outs.lower_bound(eitem(i.end - i.len, i.nt, 0, 0, 0));
 			it != outs.end() && it->first.end == i.end - i.len &&
 			it->first.nt == i.nt; ++it)
 			k = it->first,
@@ -120,6 +119,7 @@ start:	wcout << format(G, i) << endl;
 			add_item(i, k), add_item(k, j);
 		goto cont;
 	}
+	wcout << format(G, i) << endl;
 	x = g[i.alt][i.dot];
 	switch (x.t) {
 	case NULLABLE:	j = eitem(g,i.end, i.len, i.alt, i.dot + 1), add_item(i, j);
@@ -141,15 +141,15 @@ cont:	if (front.empty()) return false;
 gc:	size_t sz = s.size();
 	s.emplace(i);
 	for (const eitem& t : s)
-		if ((it = ins.find(t)) == ins.end()) continue;
-		else for (const eitem& y : it->second)
-			if ((jt=outs.find(y))!=outs.end()&&subset(jt->second,s))
-				s.emplace(y);
+		if (auto it = ins.find(t); it == ins.end())
+			for (const eitem& y : it->second)
+				if (	auto jt=outs.find(y); jt!=outs.end() &&
+					subset(jt->second,s)) s.emplace(y);
 	if (sz != s.size()) goto gc;
 	for (eitem x : s) {
-		if ((it = outs.find(x)) != outs.end())
+		if (auto it = outs.find(x); it != outs.end())
 			for (const eitem& ox : it->second) ins[ox].erase(x);
-		if ((it = ins.find(x)) != ins.end())
+		if (auto it = ins.find(x); it != ins.end())
 			for (const eitem& ix : it->second) outs[ix].erase(x);
 		ins.erase(x), outs.erase(x), front.erase(x);
 	}
@@ -159,7 +159,8 @@ gc:	size_t sz = s.size();
 
 int main(int, char**) {
 	setlocale(LC_ALL, "");
-	assert(cfg_parse(cfg_compile({{L"S",L"a"}},L"S"),L"a"));
-	assert(!cfg_parse(cfg_compile({{L"S",L"a"}},L"S"),L"aa"));
+	vector<vector<wstring>> g{{L"S",L"a"}};
+	assert(cfg_parse(cfg_compile(g,L"S"),L"a"));
+	assert(!cfg_parse(cfg_compile(g,L"S"),L"aa"));
 	return 0;
 }
