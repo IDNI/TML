134a9461e6aba503503fd905e15576b031cc9217
Ohad Asor <ohad@idni.org>
2018-03-24 01:40:57 +0100

    support nonlinear terms

--- parser/tset.cpp
+++ parser/tset.cpp
@@ -1,102 +1,81 @@
-#include <vector>
-#include <set>
-#include <cstring>
-#include <array>
-#include <iostream>
-#include <cassert>
-using namespace std;
+#include "tset.h"
 
-int icmp(const int32_t *x, const int32_t *y, size_t len) {
-	for (size_t n = 0; n < len; ++n)
-		if (x[n] != y[n])
-			return x[n] < y[n] ? -1 : 1;
+vec buf = (vec)memset(new int_t[max_arity], 0, sizeof(int_t) * max_arity);
+
+int icmp(vecc x, vecc y, size_t len) {
+	for (size_t n=0;n<len;++n) if (x[n]!=y[n]) return x[n]<y[n] ? -1 : 1;
 	return 0;
 }
+int tset::icmp(vecc x, vecc y) const { return ::icmp(x, y, arity); }
 
-class tset { // tuple set
-	size_t arity;
-	struct cmp {
-		const size_t k, a;
-		cmp(size_t k, size_t a) : k(k), a(a) {}
-		bool operator()(const int32_t* x, const int32_t* y) {
-			return x[k] != y[k] ? x[k] < y[k] : icmp(x, y, a) < 0;
-		}
-	};
-	set<const int32_t*, cmp> **s;
-	int32_t* buf;
-	set<const int32_t*>::const_iterator first(size_t n, int32_t i) {
-		buf[n] = i;
-		auto it = s[n]->lower_bound(buf);
-		return buf[n] = 0, it;
-	}
-	set<const int32_t*>::const_iterator last(size_t n, int32_t i) {
-		++(buf[n] = i);
-		auto it = s[n]->upper_bound(buf);
-		return buf[n] = 0, it;
-	}
-public:
-	typedef set<const int32_t*>::const_iterator set_iter;
-	typedef pair<size_t, array<set_iter, 2>> iter_pair;
-	typedef vector<iter_pair> iters;
+bool tset::cmp::operator()(vecc x, vecc y) const {
+	return x[k] != y[k] ? x[k] < y[k] : ::icmp(x, y, a) < 0;
+}
 
-	tset(size_t arity): arity(arity),s(new set<const int32_t*,cmp>*[arity]),
-		buf(new int32_t[arity]) {
-		for (size_t n = 0; n < arity; ++n)
-			s[n] = new set<const int32_t*, cmp>(cmp(n, arity));
-		memset(buf, 0, sizeof(int32_t) * arity);
-	}
+tset::set_iter tset::first(size_t n, int_t i) const {
+	auto it = s[n]->lower_bound((buf[n] = i, buf));
+	return buf[n] = 0, it;
+}
 
-	void add(const int32_t* t){for(size_t n=0;n<arity;++n)s[n]->emplace(t);}
+tset::tset(size_t arity): arity(arity), s(new set<vecc,cmp>*[arity]) {
+	for(size_t n = 0; n < arity; ++n) s[n]=new set<vecc,cmp>(cmp(n,arity));
+}
+void tset::add(vecc t) { for(size_t n = 0; n < arity; ++n) s[n]->emplace(t); }
 
-	const int32_t* first(const int32_t* pat, iters& it) {
-		for (size_t n = 0; n < arity; ++n)
-			if (pat[n] < 0) continue;
-			else if (auto lb=first(n, pat[n]); lb!=s[n]->end())
-					it.emplace_back(iter_pair{n, 
-						{lb, last(n, pat[n])}});
-			else return 0;
-		return next(it);
-	}
+vecc tset::first(vecc pat, iters& it) const {
+	for (size_t n = 0; n < arity; ++n)
+		if (pat[n] < 0) continue;
+		else if (auto lb = first(n, pat[n]); lb != s[n]->end())
+			it.push_back({n, {lb, first(n, pat[n] + 1)}});
+		else return 0;
+	return next(pat, it);
+}
 
-	const int32_t* next(iters& it) {
-	loop:	bool b = false;
-		if (it[0].second[0] == it[0].second[1]) return 0;
-		for (size_t n = 1; n < it.size(); ++n) {
-			auto &jt = it[n].second[0], &kt = it[n-1].second[0];
-			if (jt == it[n].second[1]) return 0;
-			if (icmp(*jt, *kt, arity) < 0) {
-				b = true;
-				if ((jt = s[it[n].first]->lower_bound(*kt)) ==
-					s[it[n].first]->end())
-					return 0;
-			}
-			if (icmp(*jt, *kt, arity) > 0) {
-				b = true;
-				if ((kt = s[it[n-1].first]->lower_bound(*jt))==
-					s[it[n-1].first]->end())
-					return 0;
-			}
-		}
-		if (!b) return *(it[0].second[0]++);
-		goto loop;
-	}
-};
+bool rep(env& e, int_t v, int_t c) {
+	if (auto it = e.find(v); it == e.end())
+		return e.emplace(v, c), true;
+	else return it->second == c;
+}
+
+#define has(it, x, y) ((it = (x).lower_bound(y)) != (x).end())
+vecc tset::next(vecc pat, iters& it) const {
+	int c;
+	vecc r;
+loop:	bool b = false;
+	env e;
+	if (it[0].second[0] == it[0].second[1]) return 0;
+	for (size_t n = 1; n < it.size(); ++n)
+		if (auto &jt = it[n].second[0], &kt = it[n-1].second[0];
+			jt == it[n].second[1] ||
+			((c = icmp(*jt, *kt)) < 0 && (b = true) &&
+				!has(jt, *s[it[n].first], *kt)) ||
+			(c > 0 && (b = true) && !has(kt,*s[it[n-1].first],*jt)))
+			return 0;
+	if (b) goto loop;
+	r = *(it[0].second[0]++);
+	for (size_t n = 0; n < arity; ++n)
+		if (pat[n] < 0 && !rep(e, pat[n], r[n]))
+			goto loop;
+	return r;
+}
 
 int main(int, char**) {
 	tset s(3);
-	int32_t a[]={1,2,3};
-	int32_t b[]={1,2,4};
-	int32_t c[]={1,2,4};
-	int32_t d[]={1,1,3};
-	int32_t e[]={4,3,4};
-	int32_t q[]={1,2,-4};
-	const int32_t *r;
+	int_t a[]={1,2,3};
+	int_t b[]={1,2,2};
+	int_t c[]={1,2,4};
+	int_t d[]={1,1,3};
+	int_t e[]={4,3,4};
+	int_t q[]={1,-4,-4};
+	//	cin >> q[0] >> q[1] >> q[2];
+	//	cin >> a[0] >> a[1] >> b[2];
+	vecc r;
 	s.add(a), s.add(b), s.add(c), s.add(d), s.add(e);
 	tset::iters it;
 	assert(r = s.first(q, it));
 	for (size_t n = 0; n < 3; ++n) cout << r[n] << ' ';
 	cout << endl;
-	while ((r = s.next(it))) {
+	while ((r = s.next(q, it))) {
 		for (size_t n = 0; n < 3; ++n) cout << r[n] << ' ';
 		cout << endl;
 	}
