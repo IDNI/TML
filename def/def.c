b9685ee2d4e153c3e5a68f3cc69f4a7f4910eac7
Ohad Asor <ohad@idni.org>
2018-08-02 13:48:10 +0200

    intermediate

--- def/def.c
+++ def/def.c
@@ -1,12 +1,44 @@
-#include "def.h"
-#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <wctype.h>
+#include <wchar.h>
+#include <locale.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
 
-dict_t *dict = 0;
-stack_t *stack = 0;
+#define int_t				intptr_t
+#define INT_T_ERR			((int_t)-1)
+#define new(x)				((x*)malloc(sizeof(x)))
+#define resize(x,t,l)			((t*)((x)=realloc(x,sizeof(t)*(l))))
+#define array_append(a, t, l, x)	(++l, (((t*)resize(a, t, l))[l-1] = (x)))
+#define array_append2(a,t,b,s,l,x,y)	(++l, (((t*)resize(a, t, l))[l-1] = (x)), (((s*)resize(b, s, l))[l-1] = (y)))
+#define same_term(x, y)			((x).ar == (y).ar && !memcmp((x).t, (y).t, sizeof(int_t)*((x).ar+1)))
+//#define term_create(_t, _ar) 		((term){.t=_t, .ar=_ar})
+#define str_from_id(id)			(id < 0 ? gconsts[-id-1] : gvars[id-1])
+#define str_to_id(s, n)			_str_to_id(&dict, s, n)
+#define clause_clear(c)			((c).terms ? free((c).terms), (c).terms=0, (c).sz=0 : 0)
+#define usage 	"Usage: <relation symbol> <src filename> <dst filename>\n"  \
+		"Will output the program after plugging src into dst.\n)"
+typedef const wchar_t* ws;
+
+
+struct dict_t { // used to store unique strings, map them to ids, and map to ids to general purpose void*
+	const wchar_t* s;	// ptr to the string's beginning
+	uint32_t h;		// hash
+	int32_t id;		// given id, positive for vars, negative for consts
+	size_t n;		// length of the string
+	struct dict_t *l, *r;	// next in hashtable
+} *dict = 0;
+
+typedef struct { int_t *t;	size_t ar;	} term;
+typedef struct { term *terms;	size_t sz;	} clause;
+
+typedef struct dict_t dict_t;
 wchar_t **gconsts = 0, **gvars = 0;
-int_t *vardata = 0;
-constdata *cdata = 0;
 size_t gnconsts = 0, gnvars = 0;
+bool clause_add_term(clause *c, const term t);
 
 uint32_t hash(const wchar_t* s, size_t n) {
 	uint32_t h = 1;
@@ -19,103 +51,21 @@ int_t _str_to_id(dict_t **d, const wchar_t* s, size_t n) {
 	if (!*d) {
 		if (*s == L'?')
 			return	array_append(gvars, const wchar_t*, gnvars, s),
-				resize(vardata, void*, gnvars), vardata[gnvars-1] = 0,
-				(*(*d = new(dict_t)) = (dict_t){ .s=s, .h=h, .id = gnvars, .l=0, .r=0, .n=n }).id;
+				(*(*d = new(dict_t)) =
+					(dict_t){.s=s,.h=h,.id = gnvars,.l=0,.r=0,.n=n }).id;
 		else return array_append(gconsts, const wchar_t*, gnconsts, s),
-			resize(cdata, constdata, gnconsts),
-			memset(&cdata[gnconsts-1], 0, sizeof(constdata)),
-			(*(*d = new(dict_t)) = (dict_t){ .s=s, .h=h, .id = -gnconsts, .l=0, .r=0, .n=n }).id;
+			(*(*d = new(dict_t)) =
+			 	(dict_t){.s=s,.h=h,.id=*s==L'~'?gnconsts:-gnconsts,.l=0,.r=0,.n=n}).id;
 	}
 	return	(h == (**d).h && n == (**d).n && !wmemcmp((**d).s, s, n))
-		? (**d).id
-		: _str_to_id((**d).h < h ? &(**d).l : &(**d).r, s, n);
+		? (**d).id : _str_to_id((**d).h < h ? &(**d).l : &(**d).r, s, n);
 }
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-alt* alt_create(int_t r, size_t ar) {
-	alt *a = new(alt);
-	*a = (alt){ .eq = 0, .terms = 0, .nvars = ar, .nterms = 0, .hsz = ar, .r = r };
-	if (ar) a->eq=calloc(ar, sizeof(int_t));
-	return a;
-}
-
-void alt_delete(alt* a) { if (!a) return; if (a->eq) free(a->eq); if (a->terms) free(a->terms); }
-
-void alt_add_term(alt* a, term t) {
-	if (t.ar) memset(resize(a->eq, int_t, a->nvars+t.ar)+a->nvars,0,sizeof(int_t)*t.ar);
-	array_append(a->terms, term, a->nterms, t), a->nvars += t.ar;
-}
-
-int_t alt_get_rep(alt *a, int_t v) {
-	if (v > 0) assert((size_t)v <= a->nvars);
-	else assert((size_t)-v <= gnconsts);
-	if (!a->eq || v < 0 || !a->eq[v-1]) return v;
-	if (v == a->eq[v-1]) return a->eq[v-1] = 0, v;
-	return a->eq[v-1] = alt_get_rep(a, a->eq[v-1]);
-}
-
-bool alt_add_eq(alt *a, int_t x, int_t y) {
-	x = alt_get_rep(a, x), y = alt_get_rep(a, y);
-	bool r = x==y?true:x<0?y<0?false:((a->eq[y-1]=x),true):((x>y?(a->eq[x-1]=y):(a->eq[y-1]=x)),true);
-	return r;
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-alt* alt_add_raw(int_t *h, int_t **b, size_t nh, size_t nb, size_t *sz) {
-	size_t i, j;//, r;
-	int_t d, v;
-	alt *a = alt_create(*h, nh);
-	for (i = 0, v = nh; i < nb; ++i) {
-		alt_create_term(a, *b[i], sz[i], v), v += sz[i];
-		for (j = 1; j <= sz[i]; ++j)
-			if (b[i][j] > 0) {
-				if (gnvars <= (size_t)b[i][j]) alloc_vardata(b[i][j]+1);
-				id_set_vardata(b[i][j], 0);
-			}
-	}
-	for (i = 1; i <= nh; ++i)
-		if (h[i] > 0) {
-			if (gnvars <= (size_t)h[i]) alloc_vardata(h[i]+1);
-			id_set_vardata(h[i], 0);
-		}
-	for (i = 1; i <= nh; ++i)
-		if (h[i] < 0) alt_add_eq(a, i, h[i]);
-		else if ((d = (int_t)id_get_vardata(h[i]))) alt_add_eq(a, i, d);
-		else id_set_vardata(h[i], i);
-	for (i = 0, v = nh+1; i < nb; ++i)
-		for (j = 1; j <= sz[i]; ++j, ++v)
-			if (b[i][j] < 0) alt_add_eq(a, v, b[i][j]);
-			else if ((d = (int_t)id_get_vardata(b[i][j]))) alt_add_eq(a, v, d);
-			else id_set_vardata(b[i][j], v);
-	return a;
-//	r = def_add_alt(def_get(*h, nh, src), a);
-//	return def_get(*h, nh, src)->a[r];
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-def* def_create(int_t h, size_t ar, bool src) {
-	def *d = new(def);
-	if (src) id_get_cdata(h).src = d;
-	else id_get_cdata(h).dst = d;
-	return *d = (def){ .h = term_create(h, ar, 0), .a = 0, .sz = 0 }, d;
-}
-
-def* def_get(int_t h, size_t ar, bool src) {
-	def *d = src ? id_get_cdata(h).src : id_get_cdata(h).dst;
-	return d ? d : def_create(h, ar, src);
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 wchar_t* str_read(int_t *r, wchar_t *in) {
-	wchar_t *s = in;
+	wchar_t *s = in, *t;
 	while (*s && iswspace(*s)) ++s;
 	if (!*s) return 0;
-	wchar_t *t = s;
-	if (*t == L'?') ++t;
+	if (*(t = s) == L'?' || *t == L'~') ++t;
 	while (iswalnum(*t)) ++t;
 	if (t == s) return 0;
 	*r = str_to_id(s, t - s);
@@ -123,92 +73,41 @@ wchar_t* str_read(int_t *r, wchar_t *in) {
 	return t;
 }
 
-int_t* term_read(size_t *sz, wchar_t **in) {
-	int_t x, *t = 0;
-	*sz = 0;
-	while ((*in = str_read(&x, *in))) {
-		array_append(t, int_t, *sz, x);
-		if (!iswalnum(**in) && **in != L'?') return --*sz, t;
-	}
-	return 0;
+term term_read(wchar_t **in) {
+	int_t x;
+	term r = (term){ .t = 0, .ar = 0 };
+	while ((*in = str_read(&x, *in)))
+		if (array_append(r.t, int_t, r.ar, x), !iswalnum(**in) && **in != L'?')
+			return --r.ar, r;
+	return r;
 }
 
 void id_print(int_t n) {
-	if (n > 0) {
-		wprintf(L"?%d", n);
-		return;
+	if (n > 0) wprintf(L"?%d", n);
+	else {
+		const wchar_t *s = str_from_id(n);
+		while (iswalnum(*s)) putwchar(*s++);
 	}
-	const wchar_t *s = str_from_id(n);
-	while (iswalnum(*s)) putwchar(*s++);
 }
 
-void term_print(const term t, size_t v) {
-	id_print(t.r);
-	for (size_t n = 1; n <= t.ar; ++n) wprintf(L" ?%zu", n + v);
+void term_print(const term t) {
+	for (size_t n = 0; n <= t.ar; ++n) id_print(t.t[n]), putwchar(L' ');
 }
 
-alt* alt_read(wchar_t **in) {
-	if (!*in) return 0;
-	int_t **b = 0, *h = 0, *t;
-	size_t nb = 0, nsz = 0, *sz = 0, nh = 0, s;
-	if (!(h = term_read(&nh, in))) return 0;
-//	if (src) array_append(srcs, int_t, nsrcs, *h); else array_append(dsts, int_t, ndsts, *h);
-	if (**in == L'.') return ++*in, alt_add_raw(h, 0, nh, 0, 0);
-	else if (*((*in)++) != L':') perror("':' expected\n"), exit(1);
-	while ((t = term_read(&s, in))) {
-		if (	array_append(b, int_t*, nb, t),
-			array_append(sz, size_t, nsz, s),
-			**in != L',') {
-				if (*((*in)++) != L'.') perror("'.' expected\n"), exit(1);
-				return alt_add_raw(h, b, nh, nb, sz);
-			}
-		else ++*in;
-	}
-	return 0;
-}
-
-void alt_print(alt* a) {
-	term_print(term_create(a->r, a->hsz, 0), 0);
-	size_t v = a->hsz;
-	for (size_t n = 0; n < a->nterms; ++n)
-		wprintf(L", "), term_print(a->terms[n], v), v += a->terms[n].ar;
-	wprintf(L". [");
-	int_t k;
-	for (size_t n = 1; n <= a->nvars; ++n)
-		if ((k = alt_get_rep(a, n)) > 0) { if ((size_t)k != n) wprintf(L" ?%zu = ?%lu; ", n, k); }
-		else if (k < 0) wprintf(L" ?%zu = %s; ", n, str_from_id(k));
-	wprintf(L"]\n");
-}
-
-int alt_cmp(const alt *x, const alt *y) {
-	if (x == y) return 0;
-	if (!x) return 1;
-	if (!y) return -1;
-	if (x->nterms != y->nterms) return x->nterms > y->nterms ? 1 : -1;
-	if (x->nvars != y->nvars) return x->nvars > y->nvars ? 1 : -1;
-	int r = memcmp(x->terms, y->terms, sizeof(term) * x->nterms);
-	if (r) return r;
-	assert(x->hsz == y->hsz);
-	size_t n;
-	for (n = 1; n <= x->hsz; ++n)
-		if (alt_get_rep((alt*)x, n) > 0) break;
-		else if (alt_get_rep((alt*)y, n) > 0) break;
-		else if (alt_get_rep((alt*)x, n) != alt_get_rep((alt*)y, n)) break;
-	if (n == x->hsz+1) return 0;
-	if (x->eq && y->eq) return memcmp(x->eq, y->eq, sizeof(int_t) * (x->nvars+1));
-	return x->eq > y->eq ? 1 : -1;
-}
-
-size_t def_add_alt(def *d, alt *a) {
-	for (size_t n = 0; n < d->sz; ++n) if (!alt_cmp(d->a[n], a)) return n;
-	return array_append(d->a, alt*, d->sz, a), d->sz-1;
-}
-
-void def_print(int_t t, bool src) {
-	def *d = src ? id_get_cdata(t).src : id_get_cdata(t).dst;
-	if (!d) return;
-	for (size_t n = 0; n < d->sz; ++n)
-		term_print(d->h, 0), wprintf(L": "), alt_print(d->a[n]), wprintf(L".\n");
+clause clause_read(wchar_t **in) {
+	clause c = (clause){.terms=0,.sz=0};
+	if (!*in) return c;
+	term t = term_read(in);
+	if (!t.t) return c;
+	do { clause_add_term(&c, t); } while (**in == L',' && (t = term_read(in)).t);
+	if (**in == L'.') return ++*in, c;
+	if (*((*in)++) != L':') perror("Term or ':' or ',' or '.' expected\n"), exit(1);
+	t = term_read(in);
+	if (!t.t) return clause_clear(c), c;
+	do { if (*t.t = -*t.t, !clause_add_term(&c, t)) return clause_clear(c), c;
+	} while (*((*in)++) == L',' && (t = term_read(in)).t);
+	if (*((*in)-1) != L'.') perror("Term or ',' or '.' expected\n"), exit(1);
+	return c;
 }
 
 wchar_t* file_read_text(FILE *f) {
@@ -220,7 +119,7 @@ wchar_t* file_read_text(FILE *f) {
 next:	for (n = l = 0; n < 31; ++n)
 		if (WEOF == (c = getwc(f))) { skip = false; break; }
 		else if (c == L'#') skip = true;
-		else if (c == L'\n') skip = false, buf[l++] = c;
+		else if (c == L'\r' || c == L'\n') skip = false, buf[l++] = c;
 		else if (!skip) buf[l++] = c;
 	if (n) {
 		buf[l] = 0;
@@ -230,138 +129,108 @@ next:	for (n = l = 0; n < 31; ++n)
 	return all;
 }
 
-void alt_deflate(alt *a, int_t **h, int_t ***b, size_t **sz, size_t *nb, size_t *nh) {
-	if (!a) return;
-	*h = arr(int_t, a->hsz+1), **h = a->r,
-	*b = arr(int_t*, a->nterms), *sz = arr(size_t, a->nterms);
-	*nb = a->nterms, *nh = a->hsz;
-	size_t v = 0;
-	for (size_t n = 0; n < a->hsz; ++n)
-		(*h)[n+1] = alt_get_rep(a, ++v);
-	for (size_t n = 0, m = 0; n < a->nterms; ++n, ++m) {
-		(*b)[m] = arr(int_t, a->terms[n].ar + 1);
-		*((*b)[m]) = a->terms[n].r;
-		(*sz)[m] = a->terms[n].ar+1;
-		for (size_t k = 0; k < a->terms[n].ar; ++k)
-			(*b)[n][k+1] = alt_get_rep(a, ++v);
-		for (size_t k = 0; k < n; ++k)
-			if ((*sz)[n] == (*sz)[k] && !memcmp((*b)[n], (*b)[k], (*sz)[n] * sizeof(int_t))) {
-				free((*b)[m--]);
-				break;
-			}
-	}
-}
-
-void alt_deflate_print(alt *a) {
-	if (!a) return;
-	int_t *h = 0, **b = 0;
-	size_t *sz = 0, nb, nh;
-	alt_deflate(a, &h, &b, &sz, &nb, &nh);
-	for (size_t n = 0; n <= nh; ++n) id_print(h[n]), putwchar(L' ');
-	if (nb) putwchar(L':');
-	for (size_t n = 0; n < nb; ++n) {
-		for (size_t k = 0; k < sz[n]; ++k)
-			id_print(b[n][k]), putwchar(L' ');
-		if (n+1!=nb) putwchar(L',');
-	}
+void clause_print(clause a) {
+	if (!a.terms) return;
+	size_t n, k = 0;
+	for (n = 0; n < a.sz; ++n)
+		if (*a.terms[n].t < 0)
+			term_print(a.terms[n]), putwchar(L','), ++k;
+	if (k == a.sz) return;
+	putwchar(L':');
+	for (n = 0; n < a.sz; ++n)
+		if (*a.terms[n].t > 0) {
+			*a.terms[n].t = -*a.terms[n].t;
+			term_print(a.terms[n]), putwchar(L',');
+			*a.terms[n].t = -*a.terms[n].t;
+		}
 	wprintf(L".\n");
 }
 
-void def_deflate_print(def *d) {
-	for (size_t n = 0; n < d->sz; ++n) alt_deflate_print(d->a[n]);
+bool clause_add_term(clause *c, const term t) {
+	if (c->terms)
+		for (term *x = &c->terms[0]; x != &c->terms[c->sz]; ++x)// += sizeof(term))
+			if (same_term(*x, t))
+				return -*t.t == *x->t;
+	return array_append(c->terms, term, c->sz, t), true;
 }
 
-const char usage[] = "Usage: <src filename> <dst filename>\nWill output the program after plugging src into dst.\n)";
+bool clause_compute_dst_term(const term ts, const term td, const term s, term *d) {
+	d->t = realloc(d->t, sizeof(int_t) * ((d->ar = s.ar) + 1));
+	for (size_t n = 1, k; n <= s.ar + 1; ++n) {
+		for (k = 1; k <= td.ar + 1; ++k)
+			if (td.t[k] != s.t[n] || ts.t[k] > 0) continue;
+			else {
+				if (td.t[k] < 0 && td.t[k] != ts.t[k]) return false;
+				d->t[n] = ts.t[k];
+				break;
+			}
+		if (k == td.ar+1) d->t[n] = td.t[n];
+	}
+	return true;
+}
+
+bool clause_compute_src_term(const term ts, const term td, const term s, term *d) {
+	d->t = realloc(d->t, sizeof(int_t) * ((d->ar = s.ar) + 1));
+	for (size_t n = 1, k; n <= s.ar + 1; ++n) {
+		for (k = 1; k <= td.ar + 1; ++k)
+			if (td.t[k] != s.t[n]) continue;
+			else {
+				if (ts.t[k] < 0 && td.t[k] < 0 && td.t[k] != ts.t[k])
+					return false;
+				d->t[n] = ts.t[k];
+				break;
+			}
+		if (k == td.ar+1) d->t[n] = td.t[n];
+	}
+	return true;
+}
+
+clause clause_plug(clause s, size_t ts, clause d, size_t td) {
+	clause r = (clause){ .terms = 0, .sz = 0 };
+	term t = (term) { .t = 0, .ar = 0 };
+	for (size_t n = 0; n < d.sz; ++n)
+		if (n != td && clause_compute_dst_term(s.terms[ts], d.terms[td], d.terms[n], &t))
+			clause_add_term(&r, t), t.t = 0;
+	for (size_t n = 0; n < s.sz; ++n)
+		if (n != ts && clause_compute_dst_term(d.terms[td], s.terms[ts], s.terms[n], &t))
+			clause_add_term(&r, t), t.t = 0;
+	return r;
+}
 
 int main(int argc, char** argv) {
-	int_t y, l, m;
-	size_t n, k, t, nd, ns, nsrc = 0, ndst = 0;
-	//def *ds, *dd;
-	alt *r, **src = 0, **dst = 0;
-	wchar_t *all;
-
 	setlocale(LC_CTYPE, "");
-	if (argc != 3) perror(usage), exit(1);
-	for (all = file_read_text(fopen(argv[1], "r")); all;) if ((r = alt_read(&all))) array_append(src, alt*, nsrc, r);
-	for (all = file_read_text(fopen(argv[2], "r")); all;) if ((r = alt_read(&all))) array_append(dst, alt*, ndst, r);
+	if (argc != 4) perror(usage), exit(1);
+	const size_t rlen = mbstowcs(0, argv[1], 0);
+	if (rlen == (size_t)-1) perror("Unable to read the input relation symbol."), exit(1);
 
-	t = k = n = nd = ns = 0;
-	for (nd = 0; nd < ndst; ++nd)
-	for (t = 0; t < dst[nd]->nterms; ++t)
-	for (ns = 0; ns < nsrc; ++ns) {
-	if (dst[nd]->terms[t].r != src[ns]->r || dst[nd]->terms[t].ar != src[ns]->hsz) continue;
-	alt		*da = dst[nd], *sa = src[ns];//dd->a[j], *sa = ds->a[n];
-	const int_t	dnterms = da->nterms, snterms = sa->nterms,
-			dnvars = da->nvars, snvars = sa->nvars,
-			v0 = da->terms[t].v0, ar = da->terms[t].ar, v1 = v0 + ar - 1;
-	int_t *ssub = stack_arr(int_t, ar), *dsub = stack_arr(int_t, ar);
-	alt_deflate_print(sa);
-	alt_print(sa);
-	alt_deflate_print(da);
-	alt_print(da);
-	r = alt_create(da->r, da->hsz);
+	wchar_t rsym[rlen];
+	mbstowcs(rsym, argv[1], rlen);
+	int_t r = str_to_id(rsym, rlen);
 
-	for (l = 0;	l <  dnterms;++l) if (l != (int_t)t) alt_add_term(r, da->terms[l]);
-	for (l = 0;	l <  snterms;++l) alt_add_term(r, sa->terms[l]);
-
-	for (l = v0;	l <= v1 ; ++l)
-		if ((y = alt_get_rep(da, l+1)) < 0)
-			dsub[l-v0] = y;
-
-	for (l = 0;	l <  ar ; ++l)
-		if ((y = alt_get_rep(sa, l+1)) < 0 || (y > v0 && y <= v1))
-			ssub[l] = y;
-
-	for (;		l < snvars; ++l)
-		if ((y = alt_get_rep(sa, l+1)) > 0 && y <= ar )
-			if (ssub[y-1]>=0) ssub[y-1] = l;
-
-	for (l = 1;	l <= v0;     ++l)
-		if ((y = alt_get_rep(da, l)) > v0 && y <= v1+1)
-			if (dsub[y-v0-1]>=0) dsub[y-v0-1] = l;
-
-	for (;		l < v1+1;    ++l)
-		if ((y = alt_get_rep(da, l)) <= v0 || y > v1+1)
-			dsub[l-v0-1] = y;
-
-	for (;		l <= dnvars; ++l)
-		if ((y=alt_get_rep(da,l)) > v0 && y <= v1+1)
-			if (dsub[y-v0-1]>=0) dsub[y-v0-1] = l;
-
-	for (l = 0;	l < ar; ++l) wprintf(L"ssub %d %d\n", l, ssub[l]);
-	for (l = 0;	l < ar; ++l) wprintf(L"dsub %d %d\n", l, dsub[l]);
-
-	for (l = m = 0;	l <  v0;     ++l)
-		r->eq[m++] = 	da->eq[l] > v0 && da->eq[l] <= v1+1
-				? ssub[da->eq[l-v0]] ? ssub[da->eq[l-v0]]
-				: da->eq[l] : da->eq[l];
-
-	for (l = v1+1;	l <  dnvars; ++l)
-		r->eq[m++] =	da->eq[l] > v0 && da->eq[l] <= v1+1
-				? ssub[da->eq[l-v0]] ? ssub[da->eq[l-v0]]
-				: da->eq[l] : da->eq[l];
-
-	for (l = ar+1;	l <  snvars; ++l)
-		r->eq[m++] =	sa->eq[l] <= ar && sa->eq[l] > 0
-				? dsub[sa->eq[l]-1] ? dsub[sa->eq[l]-1]
-				: sa->eq[l] < 0 ? sa->eq[l] :
-				(sa->eq[l]+dnvars-2*ar) : (sa->eq[l]+dnvars-2*ar);
-
-	for (l = v0+1;	l <= v1;     ++l)
-		if (!(y = alt_get_rep(da, l))) continue;
-		else for (m = l + 1; m <= v1; ++m)
-			if (y == alt_get_rep(da, m))
-				alt_add_eq(r, dsub[l-v0-1], dsub[m-v0-1]);
-
-	for (l = 0;	l <  ar;     ++l)
-		if (!(y = alt_get_rep(sa, l+1))) continue;
-		else for (m = l + 1; m < ar; ++m)
-			if (y == alt_get_rep(sa, m+1))
-				if (ssub[l] && ssub[m]) alt_add_eq(r, ssub[l], ssub[m]);
-
-	for (l = 0;	l < ar; ++l) if (ssub[l] && dsub[l]) alt_add_eq(r, ssub[l], dsub[l]);
-
-	alt_deflate_print(r);
-	alt_print(r);
-	}
+	clause c, d, *srcpos = 0, *srcneg = 0;
+	size_t nsrcpos = 0, nsrcneg = 0, *srcposterm = 0, *srcnegterm = 0;
+	wchar_t *all;
+	for (all = file_read_text(fopen(argv[2], "r")); all;)
+		if ((c = clause_read(&all)).terms) {
+			bool b = false;
+			for (size_t n = 0; n < c.sz; ++n)
+				if (*c.terms[n].t == r)
+					b = array_append2(srcpos, clause, srcposterm, size_t, nsrcpos, c, n);
+				else if (-*c.terms[n].t == r)
+					b = array_append2(srcneg, clause, srcnegterm, size_t, nsrcneg, c, n);
+			if (!b) clause_print(c);
+		}
+	for (all = file_read_text(fopen(argv[3], "r")); all;)
+		if ((c = clause_read(&all)).terms) {
+			bool b = false;
+			for (size_t n = 0; n < c.sz; ++n)
+				if (*c.terms[n].t == r) {
+					b = true;
+					for (size_t k = 0; k < nsrcneg; ++k)
+						if ((d = clause_plug(srcneg[k], srcnegterm[k], c, n)).terms)
+							clause_print(d);
+				}
+			if (!b) clause_print(c);
+		}
+	return 0;
 }
