03baecc44e88c4d80e08066c9260b66ce82a8597
Ohad Asor <ohad@idni.org>
2018-09-04 11:37:52 +0200

    dnf

--- dnf/dnf.cpp
+++ dnf/dnf.cpp
@@ -10,7 +10,7 @@ bool clause::has(int_t t) const {
 
 void clause::add(int_t i) {
 	if (empty()) push_back(i);
-	else if (has(-i)) clear();
+	else if (has(-i)) clear(), tau = false;
 	else if (const_iterator it = lower_bound(begin(), end(), i); (it != end() && *it != i)
 			 || (it = upper_bound(begin(), end(), i)) == end())
 		insert(it, i);
@@ -19,6 +19,7 @@ void clause::add(int_t i) {
 void clause::del(int_t i) {
 	const_iterator it = lower_bound(begin(), end(), i);
 	if (it != end() && *it == i) erase(it);
+	if (empty()) tau = true;
 }
 
 int_t clause::subclause(const clause& c) const {
@@ -43,7 +44,8 @@ pair<char, int_t> clause::subclause2(const clause& c) const {
 
 void dnf::add(clause&& c) {
 	typedef pair<size_t, pair<char, int_t>> elem;
-	if (c.empty()) { clear(); tau = true; return; }
+	if (c.tau) { clear(); tau = true; return; }
+	if (c.empty()) return;
 	vector<elem> m;
 	vector<size_t> del;
 	for (size_t n = 0; n < size(); ++n)
@@ -61,11 +63,43 @@ void dnf::add(clause&& c) {
 	add(move(c));
 }
 
+clause& clause::operator*=(const clause& c) {
+	for (int_t i : c) if (add(i); empty()) break;
+	return *this;
+}
+
+dnf clause::operator-() const {
+	dnf r;
+	for (int_t i : *this) r += clause({i});
+	return r;
+}
+
+dnf& dnf::operator+=(const dnf& d) { for (clause c : d) add(move(c)); return *this; }
+
+dnf dnf::operator*(const dnf& d) {
+	dnf r;
+	for (clause x : *this) for (const clause& y : d) r += move(x *= y);
+	return r;
+}
+
+clause clause::eq(int_t x, int_t y) const {
+	clause r;
+	for (int_t i : *this) r.add(i==x?y:i==-x?-y:i);
+	return r;
+}
+
+dnf dnf::eq(int_t x, int_t y) {
+	dnf r;
+	for (const clause& c : *this) r.add(c.eq(x, y));
+	return r;
+}
+
 wostream& operator<<(wostream& os, const clause& c) {
 	os << L'(';
 	for (int_t i : c) os << i << L' ';
 	return os << L')';
 }
+
 wostream& operator<<(wostream& os, const dnf& d) {
 	for (const clause& c : d) os << c;
 	return os;
--- dnf/dnf.h
+++ dnf/dnf.h
@@ -12,6 +12,7 @@ wostream& operator<<(wostream& os, const struct dnf&);
 struct clause : protected vector<int_t> {
 	clause(){}
 	clause(const set<int>& s);
+	bool tau = false; // whether tautology or unsat
 	void add(int_t i);
 	void del(int_t i);
 	size_t size() const { return vector<int_t>::size(); }
@@ -20,12 +21,20 @@ struct clause : protected vector<int_t> {
 	friend wostream& operator<<(wostream& os, const clause&);
 	int_t subclause(const clause& c) const;
 	pair<char, int_t> subclause2(const clause& c) const;
+	clause& operator*=(const clause& c);
+	dnf operator-() const;
+	clause eq(int_t x, int_t y) const;
 };
 
 struct dnf : protected vector<clause> {
 	friend wostream& operator<<(wostream& os, const dnf&);
-	bool tau; // whether tautology or unsat
+	dnf(){}
+	dnf(clause&& c) { add(move(c)); }
+	bool tau = false;
 	void add(clause&& c);
+	dnf& operator+=(const dnf& d);
+	dnf operator*(const dnf& d);
+	dnf eq(int_t x, int_t y);
 	size_t size() const { return vector<clause>::size(); }
 	bool empty() const { return vector<clause>::empty(); }
 };
