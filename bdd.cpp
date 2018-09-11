a20da27cf8fb348757b37d95de6aa0480e26f917
Ohad Asor <ohad@idni.org>
2018-09-11 11:39:06 +0200

    tupes to bdd

--- bdd.cpp
+++ bdd.cpp
@@ -18,6 +18,8 @@ class bdds {
 public:
 	bdds();
 	int_t from_bvec(const vector<bool>& v);
+	template<typename K> int_t from_vec(const K* v, size_t len, size_t bits);
+	template<typename K> int_t from_vec(const vector<K>& v, size_t bits);
 	void out(wostream& os, const node& n) const;
 	void out(wostream& os, size_t) const;
 	int_t bdd_or(int_t x, int_t y);
@@ -59,7 +61,24 @@ int_t bdds::bdd_and_not(int_t x, int_t y) {
 int_t bdds::from_bvec(const vector<bool>& v) {
 	int_t k = T, n = v.size() - 1;
 	do { k = v[n] ? add({n+1, k, F}) : add({n+1, F, k}); } while (n--);
+	//for (n = 0; n < v.size(); ++n) k = v[n] ? add({n+1, k, F}) : add({n+1, F, k});
 	return k;
+}
+
+template<typename K>
+int_t bdds::from_vec(const K* v, size_t len, size_t bits) {
+	int_t k = T, t = bits * len;
+	for (size_t bit = bits-1, n = len - 1;; n = len - 1) {
+		do {
+			k = v[n]&(1<<bit)? add({t--, k, F})
+					 : add({t--, F, k});
+		} while (n--);
+		if (!bit--) return k;
+	}
+}
+template<typename K>
+int_t bdds::from_vec(const vector<K>& v, size_t bits) {
+	return from_vec(&v[0], v.size(), bits);
 }
 
 void bdds::out(wostream& os, size_t n) const { out(os, V[n]); }
@@ -80,22 +99,29 @@ int_t bdds::add(const node& n) {
 
 int main() {
 	bdds b;
-	size_t t = b.from_bvec({ 1, 0, 1, 0, 1, 0, 0, 0 });
+	int_t t = b.from_bvec({ 1, 0, 1, 0, 1, 0, 0, 0 });
 	b.out(wcout, t);
 	wcout << endl;
-	size_t s = b.from_bvec({ 0, 0, 1, 1 });
+	int_t s = b.from_bvec({ 0, 0, 1, 1 });
 	b.out(wcout, s);
 	wcout << endl;
 	b.out(wcout, b.bdd_or(b.from_bvec({0,0}),b.from_bvec({1,1}))); wcout << endl;
 	b.out(wcout, b.bdd_and(b.from_bvec({0,0}),b.from_bvec({1,1}))); wcout << endl;
-	b.out(wcout, b.bdd_and_not(b.from_bvec({0,0}),b.from_bvec({1,1}))); wcout << endl;
+	// denomstrate that we assume infinitely many zeros after each string
+	// so the length is up to the user
+	b.out(wcout, b.bdd_and_not(b.from_bvec({1,1}),b.from_bvec({1,1,1}))); wcout << endl;
 	b.out(wcout, b.bdd_and_not(1, b.from_bvec({0,0}))); wcout << endl; // negation
 	bdds c; // demonstrate output to another bddset (this is a substitute for gc)
-	c.out(wcout, bdd_apply(b, 1, b, b.from_bvec({0,0}), c, op_and_not()));
-	wcout << endl;
+	c.out(wcout, bdd_apply(b, 1, b, b.from_bvec({0,0}), c, op_and_not())); wcout << endl;
+	c.out(wcout, bdd_apply(c, 1, b, b.from_bvec({0,0}), c, op_and_not())); wcout << endl;
+	c.out(wcout, bdd_apply(bdds(), 1, b, b.from_bvec({0,0}), c, op_and_not())); wcout << endl;
 //	int_t v1,v2,v3,v4;
 //	wcin >> v1 >> v2 >> v3 >> v4;
 //	b.out(wcout, b.bdd_and_not(b.from_bvec({v1,v2}),b.from_bvec({v3,v4}))); wcout << endl;
-	wcout << endl;
+	int_t x = b.from_vec<int>({1},3);
+	b.out(wcout, x); wcout << endl;
+//	int_t y = b.from_vec<int>({2,3,4},3);
+//	int_t z = b.from_vec<int>({4,5,6},3);
+//	b.out(wcout, b.bdd_or(x, b.bdd_or(y, z))); wcout << endl;
 	return 0;
 }
