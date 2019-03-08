74944d88ceba04ccb2e8ad74745226062a9e4fcf
Ohad Asor <ohad@idni.org>
2019-03-08 16:10:28 +0100

    add missing defs.h

---
+++ defs.h
@@ -0,0 +1,5 @@
+typedef int64_t int_t;
+typedef std::vector<int_t> term;
+typedef std::vector<term> matrix;// set of relational terms
+typedef std::vector<bool> bools;
+typedef std::vector<bools> vbools;
