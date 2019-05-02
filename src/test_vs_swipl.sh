2c1ed0deaa91f497c4f8e339f30ef74b1928434e
Ohad Asor <ohad@idni.org>
2019-05-02 13:03:15 +0200

    fix test script

--- src/test_vs_swipl.sh
+++ src/test_vs_swipl.sh
@@ -5,6 +5,9 @@ echo $INPUT
 ./tml --swipl < $INPUT > tml.output 2> tml.fname
 OUTPUT=`head -n 1 tml.fname | awk 'NF>1{print $NF}'`
 echo $OUTPUT
-swipl OUTPUT > swipl.output1
-cat swipl.output1 | sed -i 's/;/ /' swipl.output2
-diff swipl.output2 tml.output
+swipl $OUTPUT > swipl.output 2> /dev/null
+sed -i 's/,/ /g' swipl.output
+sed -i 's/$/./g' swipl.output
+sort swipl.output > swipl.sorted
+sort tml.output > tml.sorted
+diff swipl.sorted tml.sorted
