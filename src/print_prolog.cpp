eb3116067f3bbd39c9b038953e7015df830b7d81
Tomáš Klapka <tomas@klapka.cz>
2019-05-02 16:06:35 +0200

    print_prolog: rewrite get relarities and SWIPL's clauses order

--- src/print_prolog.cpp
+++ src/print_prolog.cpp
@@ -17,7 +17,8 @@ using namespace std;
 typedef pair<wstring, int_t> relarity;
 typedef set<relarity> relarities;
 
-void get_relarities(const raw_prog& p, relarities& all, relarities& tabling);
+relarity get_relarity(const raw_term& t);
+void get_relarities(const raw_prog& p, relarities& ras);
 wostream& output_prolog_rule(wostream& os, const raw_rule& r);
 wostream& output_prolog_term(wostream& os, const raw_term& t);
 wostream& output_prolog_elem(wostream& os, const elem& e);
@@ -25,71 +26,42 @@ wostream& output_prolog_elem(wostream& os, const elem& e);
 #define output_lexeme_adjust_first(os, l, fn) (os) << (wchar_t)fn(*((l)[0])) <<\
 	((l)[1]-((l)[0]+1)>0 ? lexeme{(l)[0]+1,(l)[1]} : lexeme{(l)[0], (l)[0]})
 
-/*wostream& driver::print_term_prolog(wostream& os, const raw_term& t) const {
-	if (t.neg) os << L'~';
-	output_lexeme_adjust_first(os, t.e[0].e, towlower);
-	os << L'(';
-	int_t skip, l = 0;
-	for (size_t ar = 0, n = 1; ar != t.arity.size();) {
-		skip = 0;
-		while (t.arity[ar] == -1) ++ar, os <<
-			(l++ && t.arity[ar-2] == 1 ? L"(" : (++skip, L""));
-		// while (t.arity[ar] == -1) ++ar, os << L'(';
-		if (n >= t.e.size()) break;
-		while (t.e[n].type == elem::OPENP) ++n;
-		for (int_t k = 0; k != t.arity[ar];)
-			if (output_lexeme_adjust_first(os, t.e[n].e, tolower),
-				++n, ++k != t.arity[ar]) os << L',';
-		while (n < t.e.size() && t.e[n].type == elem::CLOSEP) ++n;
-		++ar;
-		while (ar < t.arity.size() && t.arity[ar] == -2) ++ar,
-			os << (--l && !skip ? L")" : (--skip, L""));
-		// while (ar < t.arity.size() && t.arity[ar] == -2) ++ar, os<<L')';
-		if (ar > 1 && t.arity[ar-1] == -2 &&
-			ar != t.arity.size()) os << L",";
-
-	}
-	return os << L')';
-}*/
-
 wostream& driver::print_prolog(wostream& os, const raw_prog& p,
 	const output_dialect dialect) const {
-	relarities all, tabling, diff;
-	get_relarities(p, all, tabling);
+	relarities ras;
+	get_relarities(p, ras);
 	wstring name = dialect == SWIPL ? L"SWI Prolog" : L"XSB";
-	if (dialect == SWIPL)
-		set_difference(all.begin(), all.end(), tabling.begin(),
-			tabling.end(), inserter(diff, diff.end()));
 	os << L"% start of " << name << " program" << endl;
-	if (dialect == SWIPL) os << L":- style_check(-singleton)." << endl;
 	os << endl;
+	os << L"% enable tabling to avoid inf. loops" << endl;
+	for (auto ra : ras) os << L":- table " << ra.first <<
+		L'/' << ra.second << L'.' << endl;
+	os << endl;
+	if (dialect == SWIPL) {
+		os << L"% suppress singleton warnings" << endl;
+		os << L":- style_check(-singleton)." << endl;
+		os << endl;
+		os << L"% enable discontiguation" << endl;
+		for (auto ra : ras) os << L":- discontiguous " << ra.first <<
+			L'/' << ra.second << L'.' << endl,
+		//for (auto ra : ras)
+			os << L":- discontiguous '" << ra.first <<
+			L" tabled'" << L'/' << ra.second << L'.' << endl;
+		os << endl;
+		os << L"% declare dynamic predicates" << endl;
+		for (auto ra : ras) os << L":- dynamic '" << ra.first <<
+			L" tabled'" << L'/' << ra.second << L'.' << endl;
+		os << endl;
+	}
 	os << L"% {" << endl;
 	for (auto x : p.r) output_prolog_rule(os, x) << endl;
 	os << L"% }" << endl;
 	os << endl;
-	if (dialect == SWIPL) {
-		os << L"% enable discontiguation" << endl;
-		for (auto ra : tabling) os << L":- discontiguous " << ra.first<<
-			L'/' << ra.second << L'.' << endl;
-		os << endl;
-	}
-	os << L"% enable tabling to avoid inf. loops" << endl;
-	for (auto ra : tabling) os << L":- table " << ra.first <<
-		L'/' << ra.second << L'.' << endl;
-	os << endl;
-	if (dialect == SWIPL) {
-		os << L"% declare dynamic predicates" << endl;
-		for (auto ra : diff) os << L":- dynamic " << ra.first <<
-			L'/' << ra.second << L'.' << endl;
-		for (auto ra : tabling) os << L":- dynamic '" << ra.first <<
-			L" tabled'" << L'/' << ra.second << L'.' << endl;
-		os << endl;
-	}
 	os << L"% find all and dump to stdout" << endl;
 	os << L"dump_list([])." << endl;
 	os << L"dump_list([H|T]) :- writeln(H), dump_list(T)."<< endl;
 	os << L"dump(Q) :- findall(Q, Q, X), dump_list(X)." << endl;
-	for (auto ra : all) {
+	for (auto ra : ras) {
 		os << L":- dump(" << ra.first << L'(';
 		for(int_t i = 0; i != ra.second; ++i) os << (i ? L",_" : L"_");
 		os << L"))." << endl;
@@ -101,38 +73,24 @@ wostream& driver::print_prolog(wostream& os, const raw_prog& p,
 	return os;
 }
 
-void get_relarities(const raw_prog& p, relarities& all, relarities& tabling) {
-	wstring rel;
-	int_t ar, trigger;
-	for (auto r : p.r) {
-		for (auto t : r.h) {
-			rel = lexeme2str(t.e[0].e);
-			rel[0] = towlower(rel[0]);
-			ar = t.arity[0];
-			trigger = 0;
-			if (ar == 0)
-				for (auto a : t.arity) {
-					if (a == -1) trigger++;
-					else if (a == -2 && !--trigger) ar++;
-				}
-			relarity ra = { rel, ar };
-			all.insert(ra);
-			if (r.b.size()) tabling.insert(ra);
+relarity get_relarity(const raw_term& t) {
+	wstring rel = lexeme2str(t.e[0].e);
+	rel[0] = towlower(rel[0]);
+	int_t depth = 0, ar = t.arity[0];
+	if (ar == 0)
+		for (int_t a : t.arity) {
+			if (a == -1) depth++;
+			else if (a == -2 && !--depth) ar++;
 		}
+	return { rel, ar };
+}
+
+void get_relarities(const raw_prog& p, relarities& ras) {
+	for (auto r : p.r) {
+		for (auto t : r.h)
+			ras.insert(get_relarity(t));
 		for (auto b : r.b) for (auto t : b)
-			if (t.arity.size() == 1) {
-				ar = t.arity[0];
-				trigger = 0;
-				if (ar == 0) {
-					for (int_t a : t.arity)
-						if (a == -1) trigger++;
-						else if (a == -2 && !--trigger)
-							ar++;
-				}
-				rel = lexeme2str(t.e[0].e);
-				rel[0] = towlower(rel[0]);
-				all.insert({ rel, t.arity[0] });
-			}
+			ras.insert(get_relarity(t));
 	}
 }
 
