f32f21f92a37f602764781e289966ab826c0bcf8
castrod <david.castroesteban@gmail.com>
2022-08-03 10:05:28 +0200

    fixing minor error in factory methods

--- tests/test_transform_opt.h
+++ tests/test_transform_opt.h
@@ -19,46 +19,43 @@
 #ifndef WORK_IN_PROGRESS
 
 /* Factory methods to ease test cases building. */
-term term_f(std::vector<int_t> args) {
-	term t;	for (auto a: args ) t.emplace_back(a); return t;
-}
-
-std::vector<term> rule_f(std::vector<term> terms) {
-	std::vector<term> r; for (auto t: terms) r.emplace_back(t); return r;
-}
-
-flat_prog flat_prog_f(std::vector<std::vector<term>> terms) {
-	flat_prog fp; for (auto t: terms) fp.insert(t);	return fp;
-}
-
 int_t sym_f() {	static int_t s = 0; return ++s; }
 int_t sym_f(int_t s) {	return s < 0 ? -s: s; }
 int_t var_f() { static int_t v = 0; return --v; }
 int_t var_f(int_t v) {	return v > 0 ? -v : v; }
 
-/* Extractor methods to ease testing. */
-std::vector<term> rules_e(flat_prog fp) {
-	std::vector<term> v(fp.begin(), fp.end());
-	return v;
+term term_f(std::vector<int_t> as) {
+	term t;	
+	// add a dummy head if needed
+	if (as.empty()) t.emplace_back(sym_f());
+	else for (auto a: as ) t.emplace_back(a); 
+	return t;
 }
 
-term head_e(std::vector<term> r) {
-	return r[0];
+std::vector<term> rule_f(std::vector<term> ts) {
+	std::vector<term> r(ts.begin(), ts.end()); return r;
 }
+
+flat_prog flat_prog_f(std::vector<std::vector<term>> rs) {
+	flat_prog fp(rs.begin(), rs.end()); return fp;
+}
+
+/* Extractor methods to ease testing. */
+std::vector<std::vector<term>> rules_e(flat_prog fp) {
+	std::vector<std::vector<term>> v(fp.begin(), fp.end()); return v;
+}
+
+term head_e(std::vector<term> r) { return r[0]; }
 
 std::vector<term> body_e(std::vector<term> r) {
-	std::vector<term> v(++r.begin(), r.end());
-	return v;
+	std::vector<term> v(++r.begin(), r.end()); return v;
 }
 
 std::vector<int_t> args_e(term t) {
-	std::vector<int_t> v(++t.begin(), t.end());
-	return v;
+	std::vector<int_t> v(++t.begin(), t.end()); return v;
 }
 
-int_t pred_e(term t) {
-	return t[0];
-}
+int_t pred_e(term t) { return t[0]; }
 
 /* Check methods to ease testing. */
 bool is_var(int_t v) { return v < 0;}
