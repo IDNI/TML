1954414e85dc93d01f2adab4d3097f799b39a0ac
castrod <david.castroesteban@gmail.com>
2022-08-03 09:12:58 +0200

    adding extractors and checks to ease testing

--- tests/test_transform_opt.h
+++ tests/test_transform_opt.h
@@ -18,7 +18,7 @@
 
 #ifndef WORK_IN_PROGRESS
 
-/* Factory methods to easy test cases building */
+/* Factory methods to ease test cases building. */
 term term_f(std::vector<int_t> args) {
 	term t;	for (auto a: args ) t.emplace_back(a); return t;
 }
@@ -32,9 +32,37 @@ flat_prog flat_prog_f(std::vector<std::vector<term>> terms) {
 }
 
 int_t sym_f() {	static int_t s = 0; return ++s; }
-int_t sym_f(int_t s) {	return s; }
+int_t sym_f(int_t s) {	return s < 0 ? -s: s; }
 int_t var_f() { static int_t v = 0; return --v; }
-int_t var_f(int_t v) {	return v; }
+int_t var_f(int_t v) {	return v > 0 ? -v : v; }
+
+/* Extractor methods to ease testing. */
+std::vector<term> rules_e(flat_prog fp) {
+	std::vector<term> v(fp.begin(), fp.end());
+	return v;
+}
+
+term head_e(std::vector<term> r) {
+	return r[0];
+}
+
+std::vector<term> body_e(std::vector<term> r) {
+	std::vector<term> v(++r.begin(), r.end());
+	return v;
+}
+
+std::vector<int_t> args_e(term t) {
+	std::vector<int_t> v(++t.begin(), t.end());
+	return v;
+}
+
+int_t pred_e(term t) {
+	return t[0];
+}
+
+/* Check methods to ease testing. */
+bool is_var(int_t v) { return v < 0;}
+bool is_sym(int_t v) { return v > 0;}
 
 #endif // WORK_IN_PROGRESS
 #endif // __TEST_TRANSFORM_H__
