7708c242e88b9b079508062b96e1320812cb0a86
castrod <david.castroesteban@gmail.com>
2022-08-19 13:51:44 +0200

    adding partition iterator

--- src/iterators.h
+++ src/iterators.h
@@ -158,4 +158,104 @@ private:
 	std::vector<T> &set_;
 };
 
+/*!
+ * Iterator returning all the partitions of a given set. It uses the 
+ * grey_code_const_iterator under the hood.
+ */
+template<class T>
+class partitions_const_iterator {
+public:
+	// iterator traits
+	using difference_type = size_t;
+	using value_type = std::vector<T>;
+	using pointer = const std::vector<T>;
+	using reference = const std::vector<T>&;
+	using iterator_category = std::input_iterator_tag;
+
+	// sentinel class
+	class sentinel {
+	public:
+		bool operator==(const power_set_const_iterator<T> &that) const {
+			return that.grey_code_ == grey_code_const_iterator::end;
+		}
+
+		bool operator!=(const power_set_const_iterator<T> &that) const {
+			return !(*this == that);
+		}		
+	};
+
+	static constexpr sentinel end{};
+
+//	power_set_const_iterator(): grey_code_(0) {}
+
+	partitions_const_iterator(std::vector<T> &set): grey_code_(set.size()), set_(set) {}
+
+	partitions_const_iterator &operator++() {
+		auto delta = *(++grey_code_);
+		auto it = first_.find(delta);
+		if (it != first_.end())
+			first_.erase(it), second_.insert(delta);
+		else 
+			first_.insert(delta), second_.erase(delta);
+		return *this;
+	}
+
+	partitions_const_iterator operator++(int) {
+		auto current = *this; 
+		auto delta = *(++grey_code_);
+		auto it = first_.find(delta);
+		if (it != first_.end())
+			first_.erase(it);
+		else 
+			first_.insert(delta);
+		return current;
+	}
+
+	bool operator==(partitions_const_iterator<T> &that) const {
+		return set_ == that.set_ && first_ == that.first_ && second_ == that.second_;
+	}
+
+	bool operator!=(partitions_const_iterator<T> &that) const {
+		return !(*this == that);
+	}		
+
+	const std::pair<std::vector<T>, std::vector<T>> operator*() const{
+		std::vector<T> f(first_.begin(), first_.end());
+		std::vector<T> s(first_.begin(), first_.end());
+		return std::make_pair(f,s);
+	}
+
+private:
+	grey_code_const_iterator grey_code_;
+	std::vector<T> &set_;
+	std::set<size_t> first_;
+	std::set<size_t> second_;
+};
+
+/*!
+ * Range returning iterators to the poserset of the provided set.
+ */
+template<class T>
+class partition_range {
+public:
+
+	explicit partition_range(std::vector<T>& set) : set_(set) {}
+	
+	bool empty() {
+		return set_.size() == 0;
+	}
+
+	partitions_const_iterator<T> begin() {
+		partitions_const_iterator<T> begin(set_);
+		return begin;
+	}
+
+	partitions_const_iterator<T>::sentinel end() {
+		return partitions_const_iterator<T>::end;
+	}
+
+private:
+	std::vector<T> &set_;
+};
+
 #endif // __ITERATORS_H__
