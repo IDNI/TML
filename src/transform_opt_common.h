55f115a0c3f3805b9605f5f2574c4702fe730f4d
castrod <david.castroesteban@gmail.com>
2022-09-14 12:35:39 +0200

    moving inlined methods to header file

--- src/transform_opt_common.h
+++ src/transform_opt_common.h
@@ -26,20 +26,31 @@ using rule_index = std::map<rel_arity, std::set<flat_rule>>;
 using unification = std::map<int_t, int_t>;
 using selection = std::vector<flat_rule>;
 
-/* Get relation info from the head term in a way suitable for be used as key. */
+/*! Get relation info from the head term in a way suitable for be used as key. */
 
-inline rel_arity get_rel_info(const term &t);
+inline rel_arity get_rel_info(const term &t) {
+	return { t.tab, t.size() };
+}
 
-/* Get relation info from a flat rule in a way suitable to be used as a key. */
+/*! Get relation info from a flat rule in a way suitable to be used as a key. */
 
-inline rel_arity get_rel_info(const flat_rule &t);
+inline rel_arity get_rel_info(const flat_rule &t) {
+	return get_rel_info(t[0]);
+}
 
-/* Returns true if the vector of terms correspond to a fact, false otherwise. */
+/*! Returns true if the vector of terms correspond to a fact, false otherwise. */
 
-inline bool is_fact(const flat_rule &r);
+inline bool is_fact(const flat_rule &r) {
+	// Only one term and is not a goal
+	return r.size() == 1 && !r[0].goal;
+}
 
-/* Returns true if the vector of terms correspond to a goal, false otherwise. */
+/*! Returns true if the vector of terms correspond to a goal, false otherwise. */
 
-inline bool is_goal(const flat_rule &r);
+inline bool is_goal(const flat_rule &r) {
+	// TODO consider remove defensive programming
+	// Non empty and its a goal
+	return !r.empty() && r[0].goal;
+}
 
 #endif // __TRANSFORM_OPT_COMMON_H__
