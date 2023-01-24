ceacaa88e4423a6c52bc895724b91aae98e7df90
castrod <david.castroesteban@gmail.com>
2023-01-24 15:22:24 +0100

    fixing linking errors

--- src/utils.cpp
+++ src/utils.cpp
@@ -18,3 +18,40 @@
 #include <windows.h>
 #endif
 #include "defs.h"
+
+using namespace std;
+
+namespace idni {
+
+std::string to_string(int_t v) { return ::to_string(v); }
+
+wstring s2ws(const string& s) {
+	return wstring_convert<codecvt_utf8<wchar_t>>().from_bytes(s);
+}
+string ws2s(const wstring& s) {
+	return wstring_convert<codecvt_utf8<wchar_t>>().to_bytes(s);
+}
+wstring s2ws(const wstring& s) { return s; }
+string  ws2s(const string&  s) { return s; }
+
+wostream& operator<<(wostream& os, const string& s){ return os << s2ws(s);}
+ostream& operator<<(ostream& os, const char c) { return os.put(c); }
+
+#ifdef _WIN32
+string temp_filename() {
+	TCHAR name[MAX_PATH], path[MAX_PATH];
+	DWORD r = GetTempPath(MAX_PATH, path);
+	if (r > MAX_PATH || !r ||
+		!GetTempFileName(path, TEXT("TMLXXXX"), 0, name)) return "";
+	return string(name);
+}
+#else
+int temp_fileno() { return fileno(tmpfile()); }
+string filename(int fd) {
+        return filesystem::read_symlink(
+                        filesystem::path("/proc/self/fd") /
+                                to_string(fd));
+}
+#endif
+
+} // idni namespace
