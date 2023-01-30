813a51650b729fd6f90263cdede17a4b1a9c95ac
castrod <david.castroesteban@gmail.com>
2023-01-30 13:00:53 +0100

    moving .../tml_parser/main.cpp to tml_earley.cpp

--- src/tml_earley.cpp
+++ src/tml_earley.cpp
@@ -27,11 +27,36 @@
 #include "err.h"
 #include "ir_builder.h"
 
+#ifdef NEEDS_REWRITE
+#include "tml_parser.generated.h"
+#endif // NEEDS_REWRITE
+
 using namespace std;
 
 namespace idni {
 
 #ifdef NEEDS_REWRITE
+
+int parse_like_that() {
+	tml_parser p;
+	string line;
+	while (getline(cin, line)) {
+		if (line.size() == 0) continue;
+		cout << "> " << line << " = ";
+		auto s = from_str<char32_t>(line);
+		auto f = p.parse(s.c_str(), s.size());
+		if (!f || !p.found()) {
+			cerr << p.get_error().to_str() << endl;
+			return 1;
+		}
+		auto next_g = [](parser<char32_t>::pforest::graph &fg) {
+			auto tree = fg.extract_trees();
+			tree->to_print(cout << "\n\n------\n"), cout << endl;
+			return true;
+		};
+		f->extract_graphs(f->root(), next_g);
+	}
+}
 
 struct parsing_context {
 	raw_progs& rps;
